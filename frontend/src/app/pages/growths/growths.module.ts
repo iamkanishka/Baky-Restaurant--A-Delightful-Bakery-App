b3bbac7fe89be71c09f7777e2e1c178cef4983aa
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-14 22:01:25 +0530

    GrowthsModule

---
+++ frontend/src/app/pages/growths/growths.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GrowthsRoutingModule } from './growths-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GrowthsRoutingModule
+  ]
+})
+export class GrowthsModule { }
