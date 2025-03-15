b16c563908ec40983b22b64b204e183c8580fc81
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-15 23:45:28 +0530

    chore: Hide Action bar

--- frontend/src/app/pages/search/search.component.ts
+++ frontend/src/app/pages/search/search.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core'
+import { Page } from '@nativescript/core';
 
 @Component({
   selector: 'Search',
   templateUrl: './search.component.html',
 })
-export class SearchComponent implements OnInit {
-  constructor() {
-    // Use the constructor to inject services.
+export class SearchComponent  {
+  constructor(
+    private _page: Page,
+  ) {
+    this._page.actionBarHidden = true;
   }
 
-  ngOnInit(): void {
-    // Use the "ngOnInit" handler to initialize data for the view.
-  }
 }
