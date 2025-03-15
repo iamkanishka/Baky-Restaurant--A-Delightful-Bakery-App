045b7e45ce1cc7e7cde9fb9b8b1781ed51db359b
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-15 23:44:38 +0530

    chore: Added NativeScriptModule

--- frontend/src/app/pages/menu/menu.module.ts
+++ frontend/src/app/pages/menu/menu.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { MenuRoutingModule } from './menu-routing.module';
+import { NativeScriptModule } from '@nativescript/angular';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
+    NativeScriptModule,
     MenuRoutingModule
   ]
 })
