2abfcf15f0e39cc076f21d1bfad54ae7f9cbeebd
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-15 23:44:17 +0530

    chore: Removed unused imports

--- frontend/src/app/pages/menu/menu.component.ts
+++ frontend/src/app/pages/menu/menu.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
