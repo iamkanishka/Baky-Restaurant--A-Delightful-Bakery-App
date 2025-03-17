5a54c08036f8e6e605fb3f3290a88c6e4bcac6d7
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-17 21:48:09 +0530

    DiscountsComponent

---
+++ frontend/src/app/pages/discounts/discounts.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DiscountsRoutingModule } from './discounts-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DiscountsRoutingModule
+  ]
+})
+export class DiscountsModule { }
