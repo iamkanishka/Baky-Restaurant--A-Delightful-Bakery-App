be4b1ee9f771da32ebc04d1997a14818595b1c1b
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-18 21:58:58 +0530

    chore: Hide Action bar

--- frontend/src/app/pages/orders/order-details/order-details.component.ts
+++ frontend/src/app/pages/orders/order-details/order-details.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { Page } from '@nativescript/core';
 
 @Component({
   selector: 'app-order-details',
-  imports: [],
+  
   templateUrl: './order-details.component.html',
   styleUrl: './order-details.component.scss'
 })
 export class OrderDetailsComponent {
-
+  constructor(private _page: Page) {
+    this._page.actionBarHidden = true;
+  }
 }
