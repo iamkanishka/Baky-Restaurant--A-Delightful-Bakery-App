598066c416cc43032c55f9e485e94522c89262ad
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-18 21:58:53 +0530

    Declared Orders Comp

--- frontend/src/app/pages/orders/orders.module.ts
+++ frontend/src/app/pages/orders/orders.module.ts
@@ -3,9 +3,10 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "@nativescript/angular";
 
 import { OrdersRoutingModule } from "./orders-routing.module";
+import { OrdersComponent } from "./orders.component";
 
 @NgModule({
-  declarations: [],
+  declarations: [OrdersComponent],
   imports: [NativeScriptModule, OrdersRoutingModule],
   schemas: [NO_ERRORS_SCHEMA],
   
