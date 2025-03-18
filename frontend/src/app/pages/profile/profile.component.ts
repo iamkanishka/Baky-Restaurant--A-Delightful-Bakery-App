f9fe0d9a60212adfde9ea9eda7bb76d49f400f5b
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-18 21:59:10 +0530

    chore: Hide Action bar

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { Page } from '@nativescript/core';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
-
+    constructor(private _page: Page) {
+      this._page.actionBarHidden = true;
+    }
 }
