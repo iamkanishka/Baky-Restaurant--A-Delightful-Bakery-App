dfb521b8ef7d8075e3eb3b93ede2192933f5d694
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-18 21:58:38 +0530

    chore: Hide Action bar

--- frontend/src/app/pages/orders/orders-routing.module.ts
+++ frontend/src/app/pages/orders/orders-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Page } from '@nativescript/core';
 
 const routes: Routes = [];
 
@@ -7,4 +8,10 @@ const routes: Routes = [];
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class OrdersRoutingModule { }
+export class OrdersRoutingModule { 
+
+    constructor(private _page: Page) {
+      this._page.actionBarHidden = true;
+    }
+
+}
