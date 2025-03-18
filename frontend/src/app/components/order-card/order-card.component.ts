ceaaf5027b74c9a25a0b68e075700b5041a3c323
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-18 21:58:00 +0530

    chore: Remove unused Imports

--- frontend/src/app/components/order-card/order-card.component.ts
+++ frontend/src/app/components/order-card/order-card.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-order-card',
-  imports: [],
+  
   templateUrl: './order-card.component.html',
   styleUrl: './order-card.component.scss'
 })
