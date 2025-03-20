6a1faaf2b1ae3c1aee4fd6380af5701544e578fe
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-20 21:36:52 +0530

    chore: Added DiscountsModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
+import { DiscountsModule } from './discounts/discounts.module';
 
 @Module({
   
   controllers: [AppController],
   providers: [AppService],
-  imports: [OrdersModule],
+  imports: [OrdersModule, DiscountsModule],
 })
 export class AppModule {}
