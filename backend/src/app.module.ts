ddcdee36a39aa16f2c541b1d818307684abdf74c
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-21 21:43:27 +0530

    chore: Added MenuModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 import { DiscountsModule } from './discounts/discounts.module';
+import { MenuModule } from './menu/menu.module';
 
 @Module({
   
   controllers: [AppController],
   providers: [AppService],
-  imports: [OrdersModule, DiscountsModule],
+  imports: [OrdersModule, DiscountsModule, MenuModule],
 })
 export class AppModule {}
