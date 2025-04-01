a5dc6b1478cd91c6751af2703ae90f422ce233bc
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-04-01 21:37:20 +0530

    chore: GrowthModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,11 +4,12 @@ import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 import { DiscountsModule } from './discounts/discounts.module';
 import { MenuModule } from './menu/menu.module';
+import { GrowthModule } from './growth/growth.module';
 
 @Module({
   
   controllers: [AppController],
   providers: [AppService],
-  imports: [OrdersModule, DiscountsModule, MenuModule],
+  imports: [OrdersModule, DiscountsModule, MenuModule, GrowthModule],
 })
 export class AppModule {}
