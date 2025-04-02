972f4ce477fbc1b48b6632e62b3bef9db5ea1038
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-04-02 21:30:09 +0530

    AuthModule Added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,11 +7,12 @@ import { MenuModule } from './menu/menu.module';
 import { GrowthModule } from './growth/growth.module';
 import { InsightModule } from './insight/insight.module';
 import { ProfileModule } from './profile/profile.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   
   controllers: [AppController],
   providers: [AppService],
-  imports: [OrdersModule, DiscountsModule, MenuModule, GrowthModule, InsightModule, ProfileModule],
+  imports: [OrdersModule, DiscountsModule, MenuModule, GrowthModule, InsightModule, ProfileModule, AuthModule],
 })
 export class AppModule {}
