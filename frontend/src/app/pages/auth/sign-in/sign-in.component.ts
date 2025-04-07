1d19576ed3254d25416b1922af10b2b50d6b4170
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-04-07 23:47:53 +0530

    `Added import of @nativescript/core/Page and modified constructor to set actionBarHidden to true`

--- frontend/src/app/pages/auth/sign-in/sign-in.component.ts
+++ frontend/src/app/pages/auth/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Page } from '@nativescript/core';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './sign-in.component.scss'
 })
 export class SignInComponent {
-
+  constructor(private _page: Page) {
+    this._page.actionBarHidden = true;
+  }
 }
